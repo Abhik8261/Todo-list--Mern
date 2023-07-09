b5a3b50f5832d0fcc2c878a0bd68f3adb766222c
Abhik8261 <abhik.tech8261@gmail.com>
2023-07-09 13:20:47 +0530

    final

--- sever/app.js
+++ sever/app.js
@@ -10,7 +10,9 @@ app.use(cors());
 app.use(bodyParser.json({extended:true}));
 app.use(bodyParser.urlencoded({extended:true}));
 app.use("/",Routes);
+
+
  const PORT=8000;
   Connection();
  app.listen(PORT,()=>console.log(`server is running on ${PORT}`)) 
-  
+ 
